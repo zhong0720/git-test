2eef1686780f08872ecfcc25c6345b680732cffc
zhong0720 <zhong15858324498@163.com>
2022-07-05 23:51:37 +0800

    hot-fix commit 2

--- src/main/java/com/xiao/Hello.java
+++ src/main/java/com/xiao/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
+        System.out.println("Hello Git4222");
     }
 
 }
